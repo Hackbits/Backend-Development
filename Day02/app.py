57f6515c2e18becbbbd104e47eef7a47a09df09e
Hackbits <sridharrao764@gmail.com>
2025-09-16 01:52:42 +0530

    Day 3 Commit

--- Day02/app.py
+++ Day02/app.py
@@ -1,14 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template,request
 
 app = Flask(__name__)
 
+@app.before_request
+def log_request():
+    print(f"Incoming request: {request.method} {request.url}")
+    
+@app.after_request
+def log_response(response):
+    print(f"Outgoing response: {response.status_code}")
+    return response
+
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return render_template("home.html", person = "SRI")
+
+@app.route("/user/<name>",methods=["POST","GET"])
+def get_user(name=None):
+    if(request.method == "GET"):
+        return render_template("user.html", person = name)
+    else:
+        data = request.get_json()
+        arguments = request.args.to_dict();
+       
+        return {"name":name,"age":data['age'],"search":arguments["search"],"name":arguments["name"]},404
 
 @app.route("/about")
 def about():
-    return "This is about page. Day 2 is going well!"
+    return "This is about page. we haven't templated yet."
 
-if __name__=="main":
-    app.run(debug=True)
