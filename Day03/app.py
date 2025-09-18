106278258ba60cd573d3a07ed09449038420dd6c
Hackbits <sridharrao764@gmail.com>
2025-09-18 17:28:20 +0530

    Day 4 1st Commit

--- Day03/app.py
+++ Day03/app.py
@@ -1,10 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request,redirect,url_for, flash
 
 app = Flask(__name__)
+app.secret_key = "supersecretkey"
 
 @app.route("/")
 def home():
-    return render_template("home.html",message = "Hello from flask templates,Today is Day 3.")
+    return render_template("home.html")
+    
 
 @app.route("/base")
 def base():
@@ -13,3 +15,18 @@ def base():
 @app.route("/about")
 def about():
     return render_template("about.html")
+
+@app.route("/contact", methods=["GET", "POST"])
+def contact():
+    if request.method == "POST":
+        name = request.form.get("name")
+        email = request.form.get("email")
+        
+        if not name or not email:
+            flash("All fields are required!", "error")
+            return redirect("contact", name=name, email=email)
+        
+        flash("Message sent successfully!", "success")
+        return redirect(url_for("home"))    
+    return render_template("contact.html")
+
